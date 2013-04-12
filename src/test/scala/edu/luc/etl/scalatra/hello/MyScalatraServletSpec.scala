5dbb59d2b151acc21d2fa0fd6bf713653af49f69
Konstantin Läufer <laufer@cs.luc.edu>
2013-04-11 19:34:25 -0500

    converted to build.sbt

--- src/test/scala/edu/luc/etl/scalatra/hello/MyScalatraServletSpec.scala
+++ src/test/scala/edu/luc/etl/scalatra/hello/MyScalatraServletSpec.scala
@@ -1,6 +1,7 @@
 package edu.luc.etl.scalatra.hello
 
 import org.scalatra.test.specs2._
+import org.eclipse.jetty.servlet.ServletHolder
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
 class MyScalatraServletSpec extends ScalatraSpec { def is =
@@ -8,9 +9,11 @@ class MyScalatraServletSpec extends ScalatraSpec { def is =
     "should return status 200"                  ! root200^
                                                 end
 
-  addServlet(classOf[MyScalatraServlet], "/*")
+  addServlet(new MyScalatraServlet, "/*")
 
   def root200 = get("/") {
     status must_== 200
   }
+
+  implicit lazy val _ = new MyScalatraSwagger
 }
